8e92c383e24d88c4d2284184c5ceb8f70f70b0dd
Deepjot Grewal <deepjotgrewal50@gmail.com>
2025-09-05 01:22:14 -0700

    Moving Particle

---
+++ src/main.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <shader_s.h>
+#include <vector>
+#include <cmath>
+#include <gl/GL.h>
+
+using namespace std; 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+void circle(float x, float y, float radius);
+
+vector<float> BlackholeVertices;
+
+struct Window {
+    float width = 1000;
+    float length = 400;
+    GLFWwindow* window;
+
+    Window() {
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        window = glfwCreateWindow(width, length, "Black-Hole Simulation", NULL, NULL);
+
+        if (window == NULL)
+        {
+            cout << "Failed to create GLFW window" << endl;
+            glfwTerminate();
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            cout << "Failed to initialize GLAD" << endl;
+        }
+
+    }    
+    
+};
+
+struct Ray{
+    glm::vec3 pos;
+    unsigned int VAO, VBO;
+    glm::mat4 model = glm::mat4(1.0f);
+
+    Ray(glm::vec3 position) {
+        pos = position;
+    }
+
+    void BufferPos() {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+    
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(pos), &pos[0], GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glEnableVertexAttribArray(0);
+    }
+
+    void draw(glm::vec3 translation, Shader Obj) {
+        
+        model = glm::translate(model, translation);
+
+        Obj.mat4("model", model);
+
+
+        glEnable(GL_POINT_SIZE);
+        glPointSize(20.0f);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_POINTS, 0, 1);
+        glDisable(GL_POINT_SIZE);
+    }
+    
+};
+
+
+int main()
+{
+
+
+    
+    Window GameWindow;
+    Ray Light(glm::vec3(-1.0f,0.0f,0.0f));
+    Light.BufferPos();
+    Shader ourShader("Shaders/Shader.vs", "Shaders/Shader.fs");
+
+
+
+    circle(0.0f, 0.0f, 0.5f);
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * BlackholeVertices.size(), &BlackholeVertices[0], GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    while(!glfwWindowShouldClose(GameWindow.window))
+    {
+        processInput(GameWindow.window);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        
+        ourShader.use();
+        ourShader.mat4("model", glm::mat4(1.0f));
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, BlackholeVertices.size()/3);
+        glBindVertexArray(0);
+
+        Light.draw(glm::vec3(0.002f,0.0f,0.0f), ourShader);
+
+        glfwSwapBuffers(GameWindow.window);
+        glfwPollEvents();
+    
+    }
+
+
+    glfwTerminate();
+    return 0;
+
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+} 
+
+
+void circle(float x, float y, float radius) {
+    float centerx = x;
+    float centery = y;
+    BlackholeVertices.push_back(x);
+    BlackholeVertices.push_back(y);
+    BlackholeVertices.push_back(0.0f);
+    for (int i = 0; i <= 360; i += 5) {
+        float angle = i * 3.14159f / 180.0f;
+        x = radius * cos(angle) + centerx;
+        y = radius * sin(angle) + centery;
+        BlackholeVertices.push_back(x);
+        BlackholeVertices.push_back(y);
+        BlackholeVertices.push_back(0.0f);
+
+    }  
+}
