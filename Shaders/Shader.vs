8e68b45a1e735ec4ad827696ba35ff4551a44be9
Deepjot Grewal <deepjotgrewal50@gmail.com>
2025-09-19 00:16:13 -0700

    2D Simulation

--- Shaders/Shader.vs
+++ Shaders/Shader.vs
@@ -1,11 +1,6 @@
 #version 460 core
     layout (location = 0) in vec3 aPos;
-
-    layout (std430, binding = 1) readonly buffer ssbo {
-        mat4 modelMatrices[];
-    };
-
-    layout (std430, binding = 2) buffer sbbo1 {
+    layout (std430, binding = 1) buffer sbbo1 {
         vec4 positions[];
     };
 
@@ -15,9 +10,7 @@
     void main()
     {
         if(isInstanced){
-            vec4 position = modelMatrices[gl_InstanceID] * vec4(positions[gl_InstanceID].xyz, 1.0);
-            gl_Position = ortho * position;
-            positions[gl_InstanceID] = position;
+            gl_Position = ortho * vec4(positions[gl_InstanceID].xyz, 1.0);
         } else {
             gl_Position = ortho * vec4(aPos, 1.0);
         }
--- src/main.cpp
+++ src/main.cpp
@@ -49,38 +49,27 @@ struct Window {
 
 struct Ray{
     unsigned int VAO;
-    GLuint SSBO, SSBO2;
-    glm::mat4 model = glm::mat4(1.0f);
+    GLuint SSBO2;
     glm::vec4 color = glm::vec4(255.0f,255.0f,255.0f,1.0f);
     int amount;
     vector<glm::vec4> particles;
+    vector<glm::vec4> velocities;
     int windowLength;
-    glm::vec3 translation;
-    std::unique_ptr<glm::mat4[]> modelMatrices;
 
-    Ray(glm::vec4 Postranslation, int amountofRays, int GamewindowLength) {
+    Ray(int amountofRays, int GamewindowLength) {
         amount = amountofRays;
         windowLength = GamewindowLength;
-        translation = Postranslation;
-        modelMatrices = std::make_unique<glm::mat4[]>(amountofRays);
     }
 
     void CreateParticles() {
         for(auto [i, j] = tuple{0,0}; i < amount; i++){
             particles.push_back(glm::vec4(0.0f, j, 0.0f, 0.0f));
             j = j + float(windowLength/amount);
-
-            //if (i == 0){
-            //    model = glm::mat4(1.0f);
-            //    modelMatrices[i] = model;
-            //    continue;
-            //}
-
-            model = glm::mat4(1.0f);
-            model = glm::translate(model, translation);
-            modelMatrices[i] = model;
+            velocities.push_back(glm::vec4(0.1f, 0.0f, 0.0f, 0.0f));
 
         }
+        //particles.push_back(glm::vec4(549.930371299, 325, 0.0f, 0.0f));
+        //velocities.push_back(glm::vec4(0.25f, 0.0f, 0.0f, 0.0f));
     }
 
 
@@ -92,13 +81,7 @@ struct Ray{
         glCreateBuffers(1, &SSBO2);
         glNamedBufferStorage(SSBO2, sizeof(glm::vec4) * particles.size() , (const void*)particles.data() , GL_DYNAMIC_STORAGE_BIT);
         glBindVertexArray(VAO);
-        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, SSBO2);
-       
-
-        
-        glCreateBuffers(1, &SSBO);
-        glNamedBufferStorage(SSBO, sizeof(glm::mat4) * amount, modelMatrices.get() /*&modelMatrices[0]*/ , GL_DYNAMIC_STORAGE_BIT);
-        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, SSBO);
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, SSBO2);
 
     }
 
@@ -121,13 +104,131 @@ struct Ray{
 };
 
 
+struct Blackhole {
+    double mass, r_s, r_isco, r_p;
+    double solarMass = 1.989e30;
+    double scale = 0.00338666666;
+    double c = 299792458;
+    double G = 6.6743e-11;
+    std::unique_ptr<glm::mat4[]> modelMatrices;
+    glm::vec3 position;
+    enum : uint8_t {ACTIVE = 0, CAPTURED=1, ESCAPED=2, STUCK=3};
+    vector<uint8_t> state;
+    vector<int> steps;
+    vector<float> h_cache;
+
+    
+
+
+    Blackhole(glm::vec3 Bposition){
+        position = Bposition;
+        mass = solarMass * 10.0;
+        r_s = ((2 * G * mass)/ (c * c)) * scale;
+        r_isco = 3*r_s;
+        r_p = ((1.5f) * r_s);
+        circle(position.x, position.y, 100.0f);
+    }
+
+    glm::vec3 Force(glm::vec3 r_l, double h){
+        float total = ((-1.5f) * float(h * h));
+        glm::vec3 dis = r_l/float((std::pow(glm::length(r_l), 5.0f)));
+        
+        return dis * total;
+        
+    }
+
+    void UpdatingLight(Ray &obj){
+        float r_l;
+        glm::vec3 distance;
+
+        if (state.size() != (size_t)obj.amount){
+            state.assign(obj.amount, ACTIVE);
+            steps.assign(obj.amount, 0);
+            h_cache.assign(obj.amount, -1.0f);
+        }
+
+        for(int i = 0; i < obj.amount; i++){
+            if (state[i] == CAPTURED || state[i] == STUCK) { continue; }
+
+
+            distance = glm::vec3(obj.particles[i]) - position;
+            r_l = glm::length(distance);
+
+
+            if(r_l > r_isco || state[i] == ESCAPED){
+                if (h_cache[i] < 0.0f) {           
+                    obj.particles[i] += obj.velocities[i];
+                } else {                           
+                    state[i] = ESCAPED;
+                    obj.particles[i] += obj.velocities[i];
+                }
+                continue;
+            }
+
+            if (r_l <= r_isco){
+                if (h_cache[i] < 0.0f){
+                    h_cache[i] = glm::length(glm::cross(distance, glm::vec3(obj.velocities[i])));
+                }   
+                
+                if(r_l < r_s){
+                    state[i] = CAPTURED;
+                    //obj.particles[i] = glm::vec4(-1e6f, -1e6f, 0.0f, 0.0f);
+                    obj.velocities[i] = glm::vec4(0.0f,0.0f,0.0f,0.0f);
+                    std::cout << "stuck because radius less than r_s" << i << endl;
+                    continue;
+                }
+
+                obj.particles[i] += obj.velocities[i];
+                distance = glm::vec3(obj.particles[i]) - position;
+                r_l = glm::length(distance);
+
+                obj.velocities[i] += (glm::vec4(Force(distance, h_cache[i]),0.0f) * 40.0f);
+                steps[i]++;
+
+                if (steps[i] >= 10000) { 
+                    state[i] = STUCK;
+                    //obj.particles[i] = glm::vec4(0.0f,0.0f,0.0f,0.0f);
+                    std::cout << "stuck here" << i << endl;
+                    obj.velocities[i] = glm::vec4(0.0f,0.0f,0.0f,0.0f);
+                }
+                
+            }
+            
+        }
+
+        glNamedBufferSubData(obj.SSBO2, 0, sizeof(glm::vec4) * obj.amount, obj.particles.data());
+
+        
+    }
+
+    void circle(float x, float y, float radius) {
+        float centerx = x;
+        float centery = y;
+        BlackholeVertices.push_back(x);
+        BlackholeVertices.push_back(y);
+        BlackholeVertices.push_back(0.0f);
+        for (int i = 0; i <= 360; i += 5) {
+            float angle = i * 3.14159f / 180.0f;
+            x = radius * cos(angle) + centerx;
+            y = radius * sin(angle) + centery;
+            BlackholeVertices.push_back(x);
+            BlackholeVertices.push_back(y);
+            BlackholeVertices.push_back(0.0f);
+
+        }  
+    }
+};
+
+
 int main()
 {
+    bool firstrender = true;
     glm::vec4 BColor = glm::vec4(255.0f,0.0f,0.0f,1.0f);
     
     Window GameWindow;
+    Blackhole Blackhole(glm::vec3(700.0f, 200.0f, 0.0f));
 
-    Ray Light( glm::vec4(0.25f, 0.0f, 0.0f, 0.0f), 6, GameWindow.length);
+    Ray Light(6, GameWindow.length);
     Light.CreateParticles();
     Light.BufferPos();
 
@@ -136,9 +237,6 @@ int main()
 
     glm::mat4 ortho = glm::ortho(0.0f, float(GameWindow.width), 0.0f, float(GameWindow.length), -1.0f, 100.0f);
 
-
-
-    circle(700.0f, 200.0f, 100.0f);
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -168,7 +266,14 @@ int main()
         glDrawArrays(GL_TRIANGLE_FAN, 0, BlackholeVertices.size()/3);
         glBindVertexArray(0);
 
-        Light.draw(ourShader);
+        if (firstrender){
+            Light.draw(ourShader);
+            firstrender = false;
+        } else {
+            Blackhole.UpdatingLight(Light);
+            Light.draw(ourShader);
+        }
+        
 
         glfwSwapBuffers(GameWindow.window);
         glfwPollEvents();
@@ -192,19 +297,3 @@ void processInput(GLFWwindow *window){
 } 
 
 
-void circle(float x, float y, float radius) {
-    float centerx = x;
-    float centery = y;
-    BlackholeVertices.push_back(x);
-    BlackholeVertices.push_back(y);
-    BlackholeVertices.push_back(0.0f);
-    for (int i = 0; i <= 360; i += 5) {
-        float angle = i * 3.14159f / 180.0f;
-        x = radius * cos(angle) + centerx;
-        y = radius * sin(angle) + centery;
-        BlackholeVertices.push_back(x);
-        BlackholeVertices.push_back(y);
-        BlackholeVertices.push_back(0.0f);
-
-    }  
-}
